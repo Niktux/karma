ae38a3669ab56757383d9303cba2d49e87f858c3
Niktux <nico.ln@gmail.com>
2013-12-18 23:26:38 +0100

    Add some unit tests

--- tests/Karma/HydratorTest.php
+++ tests/Karma/HydratorTest.php
@@ -32,12 +32,20 @@ class HydratorTest extends PHPUnit_Framework_TestCase
         $this->fs->write('a.php', '');
         $this->fs->write('b.php-dist', '<%var%>');
         $this->fs->write('c.php', '<%var%>');
+        $this->fs->write('d.php-dist', 'var');
+        $this->fs->write('e.php-dist', '<%var %>');
         
         $this->hydrator->hydrate($environment);
         
         $this->assertTrue($this->fs->has('b.php'));
+        $this->assertTrue($this->fs->has('d.php'));
+        $this->assertTrue($this->fs->has('e.php'));
+        
         $this->assertSame($expectedBValue, $this->fs->read('b.php'));
+        
         $this->assertSame('<%var%>', $this->fs->read('c.php'));
+        $this->assertSame('var', $this->fs->read('d.php'));
+        $this->assertSame('<%var %>', $this->fs->read('e.php'));
     }
     
     public function providerTestSimple()
