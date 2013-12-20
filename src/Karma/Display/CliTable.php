a7b1cf40963835e5a34615ce970433b442a52dc1
Niktux <nico.ln@gmail.com>
2013-12-20 17:46:43 +0100

    #9 Implements table cli rendering

---
+++ src/Karma/Display/CliTable.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Karma\Display;
+
+class CliTable
+{
+    private
+        $values,
+        $nbColumns,
+        $columnsSize,
+        $header,
+        $rows;
+    
+    public function __construct(array $values)
+    {
+        $this->values = $values;
+        
+        $this->header = array_shift($values);
+        $this->nbColumns = count($this->header);
+        
+        $this->rows = $values;
+    }
+    
+    public function render()
+    {
+        $this->computeColumnsSize();
+        
+        $nbSeparators = $this->nbColumns + 1;
+        $paddingSize = 2 * $this->nbColumns;
+        $totalSize = array_sum($this->columnsSize) + $nbSeparators + $paddingSize;
+
+        $separatorRow = '|' . str_pad('', $totalSize - 2, '-') . '|';
+        
+        $lines = array();
+        $lines[] = $separatorRow;
+        $lines[] = $this->renderLine($this->header);
+        $lines[] = $separatorRow;
+        
+        foreach($this->rows as $row)
+        {
+            $lines[] = $this->renderLine($row);
+        }
+        
+        $lines[] = $separatorRow;
+        
+        return implode(PHP_EOL, $lines);
+    }
+    
+    private function computeColumnsSize()
+    {
+        $this->columnsSize = array_pad(array(), $this->nbColumns, -1);
+        
+        $this->updateColumnsSize(array($this->header));
+        $this->updateColumnsSize($this->rows);
+    }
+    
+    private function updateColumnsSize(array $newValues)
+    {
+        foreach($newValues as $row)
+        {
+            if(! is_array($row))
+            {
+                throw new \InvalidArgumentException('Rows must be arrays');
+            }
+            
+            if(count($row) !== $this->nbColumns)
+            {
+                throw new \InvalidArgumentException('Rows must all have the same number of columns');
+            }    
+            
+            for($i = 0; $i < $this->nbColumns; $i++)
+            {
+                $value = $this->renderValueAsString($row[$i]);
+                $length = strlen($value);
+                
+                $this->columnsSize[$i] = max($length, $this->columnsSize[$i]);
+            }
+        }
+    }
+    
+    private function renderValueAsString($value)
+    {
+        if($value === false)
+        {
+            $value = 'false';
+        }
+        elseif($value === true)
+        {
+            $value = 'true';
+        }
+        elseif($value === null)
+        {
+            $value = 'NULL';
+        }
+        
+        return (string) $value;
+    }
+    
+    private function renderLine(array $row)
+    {
+        $columns = array();
+        
+        for($i = 0; $i < $this->nbColumns; $i++)
+        {
+            $value = $this->renderValueAsString($row[$i]);
+            $columns[] = '| ' . str_pad($value, $this->columnsSize[$i], ' ') . ' ';
+        }
+        
+        return implode('', $columns) . '|';
+    }
+}
---
+++ tests/Karma/Display/CliTableTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use Karma\Display\CliTable;
+
+class CliTableTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider providerTestRender
+     */
+    public function testRender($input, $expected)
+    {
+        $table = new CliTable($input);
+        $result = $table->render();
+        
+        $this->assertSame($expected, $result);
+    }
+    
+    public function providerTestRender()
+    {
+        return array(
+            array(array(
+                array('Variable', 'Dev', 'Prod'),
+                array('x', 153, 15.24),
+                array('db.password', null, 'azertyroot1234#'),
+                array('toto', 'some_value', false),
+            ), <<<RESULT
+|--------------------------------------------|
+| Variable    | Dev        | Prod            |
+|--------------------------------------------|
+| x           | 153        | 15.24           |
+| db.password | NULL       | azertyroot1234# |
+| toto        | some_value | false           |
+|--------------------------------------------|
+RESULT
+            ),
+            array(array(
+                array('Variable', 'Dev', 'Production'),
+                array('x', 153, 15.24),
+                array('db.password', null, 'root'),
+                array('toto', '0', false),
+            ), <<<RESULT
+|---------------------------------|
+| Variable    | Dev  | Production |
+|---------------------------------|
+| x           | 153  | 15.24      |
+| db.password | NULL | root       |
+| toto        | 0    | false      |
+|---------------------------------|
+RESULT
+            ),            
+        );
+    }
+}
