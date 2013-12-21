7c72b34dec694c0afbce71edee765dc7cfb2f171
Niktux <nico.ln@gmail.com>
2013-12-21 16:55:32 +0100

    #9 Refactor

---
+++ src/Karma/Command.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Karma;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+class Command extends \Symfony\Component\Console\Command\Command
+{
+    protected
+        $app;
+    
+    protected function configure()
+    {
+        $this
+            ->addOption('confDir', null, InputOption::VALUE_REQUIRED, 'Configuration root directory', Application::DEFAULT_CONF_DIRECTORY)
+            ->addOption('master',  null, InputOption::VALUE_REQUIRED, 'Configuration master file',    Application::DEFAULT_MASTER_FILE)
+        ;
+    }
+    
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->app = new Application();
+        $this->app['configuration.path']       = $input->getOption('confDir');
+        $this->app['configuration.masterFile'] = $input->getOption('master');
+    }
+}
--- src/Karma/Command/Diff.php
+++ src/Karma/Command/Diff.php
@@ -4,7 +4,6 @@ namespace Karma\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Gaufrette\Filesystem;
 use Gaufrette\Adapter\Local;
@@ -16,19 +15,18 @@ use Karma\Configuration;
 use Karma\Configuration\ValueFilter;
 use Symfony\Component\Console\Input\InputArgument;
 use Karma\Display\CliTable;
+use Karma\Command;
 
 class Diff extends Command
 {
-    use \Karma\Logging\OutputAware;
-    
     protected function configure()
     {
+        parent::configure();
+        
         $this
             ->setName('diff')
             ->setDescription('Display differences between environment variable set')
             
-            ->addOption('confDir', null, InputOption::VALUE_REQUIRED, 'Configuration root directory', Application::DEFAULT_CONF_DIRECTORY)
-            ->addOption('master',  null, InputOption::VALUE_REQUIRED, 'Configuration master file',    Application::DEFAULT_MASTER_FILE)
             ->addArgument('env1', InputArgument::REQUIRED, 'First environment to compare')
             ->addArgument('env2', InputArgument::REQUIRED, 'Second environment to compare')
         ;
@@ -36,31 +34,26 @@ class Diff extends Command
     
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->setOutput($output);
+        parent::execute($input, $output);
         
         $environment1 = $input->getArgument('env1'); 
         $environment2 = $input->getArgument('env2'); 
         
-        $this->output->writeln(sprintf(
-            '<info>Diif between <comment>%s</comment> and <comment>%s</comment></info>',
+        $output->writeln(sprintf(
+            '<info>Diff between <comment>%s</comment> and <comment>%s</comment></info>',
             $environment1,
             $environment2
         ));
         
-        $app = new \Karma\Application();
-        $app['configuration.path']       = $input->getOption('confDir');
-        $app['configuration.masterFile'] = $input->getOption('master');
-        
-        $reader = $app['configuration'];
-        
-        $diff = $this->getDifferentValues($reader, $environment1, $environment2);
+        $diff = $this->getDifferentValues($environment1, $environment2);
         
         $table = new CliTable($diff);
         $output->writeln($table->render());
     }
     
-    private function getDifferentValues($reader, $environment1, $environment2)
+    private function getDifferentValues($environment1, $environment2)
     {
+        $reader = $this->app['configuration'];
         $values1 = $reader->getAllValuesForEnvironment($environment1);
         $values2 = $reader->getAllValuesForEnvironment($environment2);
         
