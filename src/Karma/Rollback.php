9ba44b4bf0cb98fa74561876a83ce955877abd03
Niktux <nico.ln@gmail.com>
2013-12-22 19:23:59 +0100

    #1 Add Rollback business object

---
+++ src/Karma/Rollback.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Karma;
+
+use Gaufrette\Filesystem;
+
+class Rollback
+{
+    private
+        $sources,
+        $suffix; 
+    
+    public function __construct(Filesystem $sources)
+    {
+        $this->sources = $sources;
+        $this->suffix = Application::DEFAULT_DISTFILE_SUFFIX;
+    }
+    
+    public function setSuffix($suffix)
+    {
+        $this->suffix = $suffix;
+        
+        return $this;
+    }
+    
+    public function exec()
+    {
+        $distFiles = $this->collectDistFiles();
+
+        foreach($distFiles as $file)
+        {
+            $this->rollback($file);
+        }
+    }
+    
+    private function collectDistFiles()
+    {
+        $finder = new Finder($this->sources);
+    
+        return $finder->findFiles($this->suffix);
+    }
+    
+    private function rollback($file)
+    {
+        $targetFile = substr($file, 0, strlen($this->suffix) * -1);
+        $backupFile = $targetFile . Hydrator::BACKUP_SUFFIX;
+        
+        if($this->sources->has($backupFile))
+        {
+            $backupContent = $this->sources->read($backupFile);
+            $this->sources->write($targetFile, $backupContent, true);
+        }
+    }
+}
---
+++ tests/Karma/RollbackTest.php
@@ -0,0 +1,85 @@
+<?php
+
+use Karma\Rollback;
+use Gaufrette\Filesystem;
+use Gaufrette\Adapter\InMemory;
+
+class RollbackTest extends PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->fs = new Filesystem(new InMemory());
+        
+        $this->write('a.php-dist', 'a');
+        $this->write('a.php', 'a');
+        $this->write('a.php~', 'old_a');
+        
+        $this->write('notDistFile.php', 'right');
+        $this->write('notDistFile.php~', 'wrong');
+        
+        $this->write('orphan.php~', 'wrong');
+        
+        $this->write('b.php-dist', 'b');
+        $this->write('b.php', 'b');
+        
+        $this->write('c.php-dist', 'c');
+        $this->write('c.php~', 'old_c');
+
+        $this->write('d.php-dist2', 'd2');
+        $this->write('d.php', 'd');
+        $this->write('d.php~', 'old_d');
+        
+        $this->write('subdir/s.php-dist', 's');
+        $this->write('subdir/s.php', 's');
+        $this->write('subdir/s.php~', 'old_s');
+    }
+    
+    public function testRollback()
+    {
+        $r = new Rollback($this->fs);
+        
+        $r->setSuffix('-dist')
+            ->exec();
+        
+        $shouldExists = array(
+            'a.php-dist' => 'a',
+            'a.php' => 'old_a',
+            'a.php~' => 'old_a',
+                        
+            'notDistFile.php' => 'right', // not modified !
+            'notDistFile.php~' => 'wrong',
+                        
+            'orphan.php~' => 'wrong',
+                        
+            'b.php' => 'b',
+                        
+            'c.php' => 'old_c',
+            'c.php~' => 'old_c',
+                        
+            'd.php-dist2' => 'd2',
+            'd.php' => 'd',
+            'd.php~' => 'old_d',
+                        
+            'subdir/s.php' => 'old_s',
+            'subdir/s.php~' => 'old_s',
+        );
+
+        $shouldNotExists = array('orphan.php', 'b.php~', 'd.php-dist');
+        
+        foreach($shouldExists as $f => $content)
+        {
+            $this->assertTrue($this->fs->has($f), "File $f should exists");
+            $this->assertSame($content, $this->fs->read($f));
+        }
+        
+        foreach($shouldNotExists as $f)
+        {
+            $this->assertFalse($this->fs->has($f), "File should not exists");
+        }
+    }
+    
+    private function write($name, $content = null)
+    {
+        $this->fs->write($name, $content);
+    }
+}
