37de277a06bca7beab5739d6de4ef4da3c85ef29
Niktux <nico.ln@gmail.com>
2014-02-10 13:58:04 +0100

    #16 - VCS command

--- src/Karma/Command/VCS.php
+++ src/Karma/Command/VCS.php
@@ -6,6 +6,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Karma\Command;
 use Karma\Application;
+use Symfony\Component\Console\Input\InputOption;
 
 class VCS extends Command
 {
@@ -16,7 +17,7 @@ class VCS extends Command
     {
         parent::__construct($app);
         
-        $this->vcs = $app['vcsHandler'];
+        $this->vcs = $app['vcsHandler']($app['git']);
     }
     
     protected function configure()
@@ -26,12 +27,29 @@ class VCS extends Command
         $this
             ->setName('vcs')
             ->setDescription('')
+            ->addOption('suffix', null, InputOption::VALUE_REQUIRED, 'File suffix', null)
         ;
     }
     
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         parent::execute($input, $output);
+        
+        $suffix = $input->getOption('suffix');
+        if($suffix === null)
+        {
+            $suffix = Application::DEFAULT_DISTFILE_SUFFIX;
+        
+            $profile = $this->app['profile'];
+            if($profile->hasTemplatesSuffix())
+            {
+                $suffix = $profile->getTemplatesSuffix();
+            }
+        }
+        
+        $this->output->writeln('Looking for vcs operations');
+        
+        $this->app['distFiles.suffix'] = $suffix;
         
         $this->vcs->execute();
     }
