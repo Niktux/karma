af0ae13905ebc8673ad14acd4769e04a93ea78b2
Niktux <nico.ln@gmail.com>
2014-03-05 00:10:19 +0100

    Refactor readers

--- src/Karma/Configuration/InMemoryReader.php
+++ src/Karma/Configuration/InMemoryReader.php
@@ -2,38 +2,20 @@
 
 namespace Karma\Configuration;
 
-use Karma\Configuration;
-
-class InMemoryReader implements Configuration
+class InMemoryReader extends AbstractReader
 {
     private
-        $defaultEnvironment,
-        $values,
-        $overridenVariables,
-        $customData;
+        $values;
     
     public function __construct(array $values = array())
     {
-        $this->defaultEnvironment = 'dev';
+        parent::__construct();
+        
         $this->values = $values;
-        $this->overridenVariables = array();
-        $this->customData = array();
     }
     
-    public function read($variable, $environment = null)
+    protected function readRaw($variable, $environment = null)
     {
-        $value = $this->readRaw($variable, $environment);
-        
-        return $this->handleCustomData($value);
-    }
-    
-    private function readRaw($variable, $environment = null)
-    {
-        if(array_key_exists($variable, $this->overridenVariables))
-        {
-            return $this->overridenVariables[$variable];
-        }
-        
         if($environment === null)
         {
             $environment = $this->defaultEnvironment;
@@ -49,13 +31,6 @@ class InMemoryReader implements Configuration
         return null;
     }
     
-    public function setDefaultEnvironment($environment)
-    {
-        $this->defaultEnvironment = $environment;
-        
-        return $this; 
-    }
-    
     public function getAllVariables()
     {
         $variables = array_map(function($item){
@@ -63,44 +38,5 @@ class InMemoryReader implements Configuration
         }, array_keys($this->values));
         
         return array_unique($variables);
-    }
-    
-    public function getAllValuesForEnvironment($environment = null)
-    {
-        $result = array();
-        
-        $variables = $this->getAllVariables();
-        
-        foreach($variables as $variable)
-        {
-            $result[$variable] = $this->read($variable, $environment);
-        }    
-        
-        return $result;
-    }
-    
-    public function overrideVariable($variable, $value)
-    {
-        $this->overridenVariables[$variable] = $value;
-
-        return $this;
-    }
-    
-    public function setCustomData($customDataName, $value)
-    {
-        $key = '${' . $customDataName . '}';
-        $this->customData[$key] = $value;
-        
-        return $this;
-    }
-    
-    private function handleCustomData($value)
-    {
-        if(! is_string($value))
-        {
-            return $value;
-        }
-        
-        return strtr($value, $this->customData);
     }
 }
