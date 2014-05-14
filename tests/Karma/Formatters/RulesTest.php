c8575ba0053ec599aef9a72a711ba93560b24759
Niktux <nico.ln@gmail.com>
2014-05-14 23:13:43 +0200

    Add more unit tests

--- tests/Karma/Formatters/RulesTest.php
+++ tests/Karma/Formatters/RulesTest.php
@@ -13,6 +13,8 @@ class RulesTests extends PHPUnit_Framework_TestCase
             '<true>' => 'string true',
             '<false>' => 'string false',
             '<null>' => 0,
+            'foobar' => 'barfoo',
+            'footrue' => true
         );
         
         $this->formatter = new Rules($rules);
@@ -33,15 +35,23 @@ class RulesTests extends PHPUnit_Framework_TestCase
             'boolean true' => array(true, 'string true'),
             'string true' => array('true', 'true'),
             'other string true' => array('<true>', '<true>'),
+            'footrue' => array('footrue', true),
+                        
             'boolean false' => array(false, 'string false'),
             'string false' => array('false', 'false'),
             'other string false' => array('<false>', '<false>'),
+                        
             'null' => array(null, 0),
             'string null' => array('null', 'null'),
             'other string null' => array('<null>', '<null>'),
+                        
             'zero' => array(0, 0),
             'string zero' => array('0', '0'),
             'other string zero' => array('<0>', '<0>'),
+                        
+            'foo' => array('foo', 'foo'),
+            'foobar' => array('foobar', 'barfoo'),
+            'barfoobarfoo' => array('barfoobarfoo', 'barfoobarfoo'),
         );    
     }
 }
