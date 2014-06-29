41fb9c28b8f3cceed3ce3b13ff52332eebda0695
Niktux <nico.ln@gmail.com>
2014-06-29 22:47:32 +0200

    Update version to 3.1.1

--- src/Karma/Application.php
+++ src/Karma/Application.php
@@ -15,7 +15,7 @@ use Karma\FormatterProviders\ProfileProvider;
 class Application extends \Pimple
 {
     const
-        VERSION = '3.1.0',
+        VERSION = '3.1.1',
         DEFAULT_DISTFILE_SUFFIX = '-dist',
         DEFAULT_CONF_DIRECTORY = 'env',
         DEFAULT_MASTER_FILE = 'master.conf',
