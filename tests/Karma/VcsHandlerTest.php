e446c7f087a26ed6ba0ddf47a1f73ef9b328d784
Niktux <nico.ln@gmail.com>
2014-10-28 22:04:55 +0100

    CS : trailing spaces

--- tests/Karma/VcsHandlerTest.php
+++ tests/Karma/VcsHandlerTest.php
@@ -11,7 +11,7 @@ class VcsHandlerTest extends \PHPUnit_Framework_TestCase
         $fs,
         $vcs,
         $handler;
-    
+
     protected function setUp()
     {
         $files = array(
@@ -22,24 +22,24 @@ class VcsHandlerTest extends \PHPUnit_Framework_TestCase
             'ignored.yml-dist' => null,
             'anotherFile.ini-tpl' => null,
         );
-        
+
         $this->fs = new Filesystem(new InMemory($files));
         $finder = new Finder($this->fs);
 
         $this->vcs = new \Karma\VCS\InMemory('~tracked~i', '~ignored~i');
         $this->handler = new VcsHandler($this->vcs, $finder);
     }
-    
+
     public function testExecute()
     {
         $this->handler->execute('root');
-        
+
         $expectedUntrackedFiles = array(
             'root/some/path/to/tracked.yml',
             'root/trackedAndIgnored.yml'
         );
         $this->assertSameArrayExceptOrder($expectedUntrackedFiles, $this->vcs->getUntrackedFiles());
-        
+
         $expectedIgnoredFiles = array(
             'root/a.yml',
             'root/path/to/b.yml',
@@ -47,45 +47,45 @@ class VcsHandlerTest extends \PHPUnit_Framework_TestCase
         );
         $this->assertSameArrayExceptOrder($expectedIgnoredFiles, $this->vcs->getIgnoredFiles());
     }
-    
+
     public function testRootDirIsDot()
     {
         $this->handler->execute('.');
-        
+
         $expectedUntrackedFiles = array(
             'some/path/to/tracked.yml',
             'trackedAndIgnored.yml'
         );
         $this->assertSameArrayExceptOrder($expectedUntrackedFiles, $this->vcs->getUntrackedFiles());
     }
-    
+
     public function testRootDirIsEmpty()
     {
         $this->handler->execute('');
-        
+
         $expectedUntrackedFiles = array(
             'some/path/to/tracked.yml',
             'trackedAndIgnored.yml'
         );
         $this->assertSameArrayExceptOrder($expectedUntrackedFiles, $this->vcs->getUntrackedFiles());
     }
-    
+
     private function assertSameArrayExceptOrder(array $expected, array $values)
     {
         $this->assertCount(count($expected), $values);
-        
+
         foreach($expected as $expectedValue)
         {
             $this->assertContains($expectedValue, $values);
         }
     }
-    
+
     public function testSetSuffix()
     {
         $this->handler
             ->setSuffix('-tpl')
             ->execute();
-        
+
         $this->assertSameArrayExceptOrder(array('anotherFile.ini'), $this->vcs->getIgnoredFiles());
     }
 }
