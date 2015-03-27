34ad036509d7df09af52e07cc5620e2990baa856
Niktux <nico.ln@gmail.com>
2015-03-27 22:47:25 +0100

    Bump version to 5.0.0

--- src/Karma/Application.php
+++ src/Karma/Application.php
@@ -18,7 +18,7 @@ use Karma\Generator\ConfigurationFileGenerators\YamlGenerator;
 class Application extends \Pimple
 {
     const
-        VERSION = '4.1.0',
+        VERSION = '5.0.0',
         DEFAULT_DISTFILE_SUFFIX = '-dist',
         DEFAULT_CONF_DIRECTORY = 'env',
         DEFAULT_MASTER_FILE = 'master.conf',
